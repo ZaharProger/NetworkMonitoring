c0d050d1efa5be9d4cc0987c043125d9a9eee29e
ZaharDomolego <domolegozahar@gmail.com>
2023-02-27 20:17:08 +0800

    Добавил получение расширения файла из пути до него

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from modules import app
 if __name__ == '__main__':
     parser = ArgumentParser(description='Parse arguments from cmd')
     parser.add_argument('--p', type=str, required=True)
-    parser.add_argument('--f', type=str, required=True)
     parser.add_argument('--d', type=str, required=False)
 
-    app.run(parser.parse_args())
+    args = parser.parse_args()
+    args.f = args.p.split('.')[-1]
+
+    app.run(args)
--- modules/app.py
+++ modules/app.py
@@ -1,3 +1,4 @@
+from modules.entities import HostTypes
 from modules.readers import get_reader
 
 
@@ -6,7 +7,11 @@ def run(cfg):
 
     if reader is not None:
         hosts = reader.get_data()
+
         for host in hosts:
-            print(f'[{host.host_type}] {host.name}: {host.ports}')
+            if host.host_type == HostTypes.DOMAIN:
+                pass
+            elif host.host_type == HostTypes.IP:
+                pass
     else:
-        print(f'Модуль для обработки файлов {cfg.f} не найден!')
+        print(f"Модуль для обработки файлов {cfg.f} не найден!")
